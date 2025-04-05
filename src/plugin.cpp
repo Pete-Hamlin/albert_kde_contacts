e9d713bdfb06d51c3752d273a144dd1e0273a912
pete <work@pete-hamlin.com>
2025-04-05 21:15:34 +0100

    feat (WIP): Added akonadi deps and basic integration

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,11 +1,17 @@
 #include "plugin.h"
 #include "configwidget.h"
+#include <Akonadi/Collection>
+#include <Akonadi/CollectionFetchJob>
+#include <Akonadi/CollectionFetchScope>
+#include <Akonadi/Item>
+#include <Akonadi/ItemFetchJob>
+#include <Akonadi/ItemFetchScope>
+#include <QMessageBox>
+#include <QSettings>
+#include <albert/albert.h>
 #include <albert/logging.h>
 #include <albert/notification.h>
 #include <albert/standarditem.h>
-#include <albert/util.h>
-#include <chrono>
-#include <thread>
 
 ALBERT_LOGGING_CATEGORY("contacts_kde")
 
@@ -14,33 +20,59 @@ using namespace std;
 
 static auto icon = {QStringLiteral("qsp:SP_MessageBoxWarning")};
 
-// const QStringList Plugin::icon_urls = {"xdg:calc", ":qalculate"};
+// const QStringList Plugin::icon_urls = {"xdg:contact"};
 
-Plugin::Plugin() {}
+Plugin::Plugin() {
+  auto s = settings();
 
-Plugin::~Plugin() { DEBG << "'Debug' destroyed."; }
+  connect(this, &Plugin::collectionsChanged, this, &Plugin::updateIndexItems);
+
+  updateCollectionList();
+}
+
+// Plugin::~Plugin() {}
 
 QWidget *Plugin::buildConfigWidget() { return new ConfigWidget; }
 
-void Plugin::updateCollectionList() {}
+void Plugin::updateCollectionList() {
 
-void Plugin::handleTriggerQuery(albert::Query *query) {
-  if (query->string() == QStringLiteral("busy")) {
-    for (int i = 0; query->isValid() && i < 3; ++i) {
-      for (int nap = 0; nap < 100; ++nap) {
-        if (!query->isValid())
-          return;
-        this_thread::sleep_for(chrono::milliseconds(10));
-      }
-      query->add(albert::StandardItem::make(
-          {}, QString("Item #%1").arg(i), QString("Wow, Item #%1").arg(i), icon,
-          {{"Debug", "Open website", []() { albert::openWebsite(); }}}));
-    }
-    return;
-  }
+  debug(tr("Fetching contact collections from akonadi"));
+  // Create a fetch job to list all collections
+  Akonadi::CollectionFetchJob *fetchJob = new Akonadi::CollectionFetchJob(
+      Akonadi::Collection::root(), Akonadi::CollectionFetchJob::Recursive);
 
-  if (QStringLiteral("busy").startsWith(query->string())) {
-    query->add(albert::StandardItem::make({}, "busy", "Test delayed queries",
-                                          "debug busy", icon, {}));
-  }
+  QObject::connect(
+      fetchJob, &Akonadi::CollectionFetchJob::result, [this](KJob *job) {
+        if (job->error()) {
+          return error(
+              tr("Error fetching collections: %1").arg(job->errorString()));
+        }
+
+        collections_.clear();
+        Akonadi::CollectionFetchJob *fetchJob =
+            qobject_cast<Akonadi::CollectionFetchJob *>(job);
+        const Akonadi::Collection::List collections = fetchJob->collections();
+        QList<Akonadi::Collection> contactCollections;
+        for (const Akonadi::Collection &collection : collections) {
+          if (collection.contentMimeTypes().contains(
+                  QString::fromUtf8("text/directory"))) {
+            // TODO: parse collectionList here
+            collections_.emplace_back(collection, name(), collection.id());
+            debug(tr("Collection: %1").arg(collection.name()));
+          }
+        }
+      });
+
+  emit collectionsChanged();
+}
+
+void Plugin::debug(const QString &msg) {
+  DEBG << msg;
+  emit statusInfo(msg);
+}
+
+void Plugin::error(const QString &msg, QWidget *modal_parent) {
+  WARN << msg;
+  emit statusInfo(msg);
+  QMessageBox::warning(modal_parent, qApp->applicationDisplayName(), msg);
 }
--- src/plugin.h
+++ src/plugin.h
@@ -1,16 +1,28 @@
 #pragma once
+#include "Collection.h"
 #include <albert/extensionplugin.h>
-#include <albert/triggerqueryhandler.h>
+#include <albert/indexqueryhandler.h>
 
 class Plugin : public albert::ExtensionPlugin,
-               public albert::TriggerQueryHandler {
+               public albert::IndexQueryHandler {
   ALBERT_PLUGIN
 public:
   Plugin();
-  ~Plugin() override;
+  // ~Plugin() override;
 
-  void updateCollectionList();
+  QString defaultTrigger() const override { return QStringLiteral("cn "); }
   QWidget *buildConfigWidget() override;
 
-  void handleTriggerQuery(albert::Query *) override;
+  void updateCollectionList();
+  void updateIndexItems() override;
+
+private:
+  void debug(const QString &);
+  void error(const QString &, QWidget *modal_parent = nullptr);
+
+  std::vector<Collection> collections_;
+
+signals:
+  void collectionsChanged();
+  void statusInfo(const QString &);
 };
